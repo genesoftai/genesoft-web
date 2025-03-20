380f106b620abade9c8adb5c8add01a11568ccd5
khemmapichpanyana <khemmapich@gmail.com>
2025-03-20 13:23:38 +0700

    feature: implement integration so user can add firebase and stripe keys

--- src/components/project/services/FirebaseSection.tsx
+++ src/components/project/services/FirebaseSection.tsx
@@ -5,15 +5,97 @@ import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
 import FirebaseLogo from "@public/firebase/Firebase-logo-02.png";
 import Image from "next/image";
+import { FirebaseEnv } from "@/types/integration";
+import { Button } from "@/components/ui/button";
+import { updateEnvs } from "@/actions/integration";
+import { Loader2 } from "lucide-react";
 
-export const FirebaseSection = () => {
-    const [apiKey, setApiKey] = useState("");
-    const [authDomain, setAuthDomain] = useState("");
-    const [projectId, setProjectId] = useState("");
-    const [storageBucket, setStorageBucket] = useState("");
-    const [messagingSenderId, setMessagingSenderId] = useState("");
-    const [appId, setAppId] = useState("");
-    const [measurementId, setMeasurementId] = useState("");
+type FirebaseSectionProps = {
+    projectId: string;
+    firebaseEnv: FirebaseEnv;
+};
+
+export const FirebaseSection = ({
+    projectId,
+    firebaseEnv,
+}: FirebaseSectionProps) => {
+    const [apiKey, setApiKey] = useState(
+        firebaseEnv?.NEXT_PUBLIC_FIREBASE_API_KEY?.value,
+    );
+    const [authDomain, setAuthDomain] = useState(
+        firebaseEnv?.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN?.value,
+    );
+    const [firebaseProjectId, setFirebaseProjectId] = useState(
+        firebaseEnv?.NEXT_PUBLIC_FIREBASE_PROJECT_ID?.value,
+    );
+    const [storageBucket, setStorageBucket] = useState(
+        firebaseEnv?.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET?.value,
+    );
+    const [messagingSenderId, setMessagingSenderId] = useState(
+        firebaseEnv?.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID?.value,
+    );
+    const [appId, setAppId] = useState(
+        firebaseEnv?.NEXT_PUBLIC_FIREBASE_APP_ID?.value,
+    );
+    const [measurementId, setMeasurementId] = useState(
+        firebaseEnv?.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID?.value,
+    );
+
+    const [loading, setLoading] = useState(false);
+
+    const handleSaveChanges = async () => {
+        setLoading(true);
+        console.log({
+            message: "handleSaveChanges",
+            apiKey,
+            authDomain,
+            projectId: firebaseProjectId,
+            storageBucket,
+            messagingSenderId,
+            appId,
+            measurementId,
+        });
+
+        const payload = {
+            project_id: firebaseProjectId,
+            env_vars: {
+                NEXT_PUBLIC_FIREBASE_API_KEY: apiKey,
+                NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN: authDomain,
+                NEXT_PUBLIC_FIREBASE_PROJECT_ID: firebaseProjectId,
+                NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET: storageBucket,
+                NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID: messagingSenderId,
+                NEXT_PUBLIC_FIREBASE_APP_ID: appId,
+                NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID: measurementId,
+            },
+            branch: "dev",
+            target: ["preview"],
+            env_vars_comment: {
+                NEXT_PUBLIC_FIREBASE_API_KEY: "Firebase API Key",
+                NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN: "Firebase Auth Domain",
+                NEXT_PUBLIC_FIREBASE_PROJECT_ID: "Firebase Project ID",
+                NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET: "Firebase Storage Bucket",
+                NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID:
+                    "Firebase Messaging Sender ID",
+                NEXT_PUBLIC_FIREBASE_APP_ID: "Firebase App ID",
+                NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID: "Firebase Measurement ID",
+            },
+        };
+        try {
+            const response = await updateEnvs({
+                ...payload,
+                project_id: projectId,
+            });
+            console.log({
+                message: "handleSaveChanges",
+                response,
+            });
+        } catch (error) {
+            console.error("Error updating firebase envs:", error);
+            throw new Error("Failed to update firebase envs");
+        } finally {
+            setLoading(false);
+        }
+    };
 
     return (
         <div className="space-y-3">
@@ -72,8 +154,8 @@ export const FirebaseSection = () => {
                     id="firebase-project-id"
                     placeholder="nextjs-firebase-web-template"
                     className="bg-secondary-dark border-line-in-dark-bg text-white text-sm"
-                    value={projectId}
-                    onChange={(e) => setProjectId(e.target.value)}
+                    value={firebaseProjectId}
+                    onChange={(e) => setFirebaseProjectId(e.target.value)}
                 />
             </div>
             <div className="space-y-2">
@@ -133,6 +215,17 @@ export const FirebaseSection = () => {
                     onChange={(e) => setMeasurementId(e.target.value)}
                 />
             </div>
+
+            <Button
+                className="bg-genesoft hover:bg-genesoft/90 text-white text-sm"
+                onClick={handleSaveChanges}
+                disabled={loading}
+            >
+                {loading ? "Saving..." : "Save Changes"}
+                {loading && (
+                    <Loader2 className="w-4 h-4 ml-2 text-white animate-spin" />
+                )}
+            </Button>
         </div>
     );
 };
--- src/components/project/services/StripeSection.tsx
+++ src/components/project/services/StripeSection.tsx
@@ -5,10 +5,63 @@ import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
 import StripeLogo from "@public/stripe.jpg";
 import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { updateEnvs } from "@/actions/integration";
+import { StripeEnv } from "@/types/integration";
+import { Loader2 } from "lucide-react";
 
-export const StripeSection = () => {
-    const [secretKey, setSecretKey] = useState("");
-    const [webhookSecret, setWebhookSecret] = useState("");
+type StripeSectionProps = {
+    projectId: string;
+    stripeEnv: StripeEnv;
+};
+
+export const StripeSection = ({ projectId, stripeEnv }: StripeSectionProps) => {
+    const [secretKey, setSecretKey] = useState(
+        stripeEnv?.STRIPE_SECRET_KEY?.value,
+    );
+    const [webhookSecret, setWebhookSecret] = useState(
+        stripeEnv?.STRIPE_WEBHOOK_SECRET?.value,
+    );
+
+    const [loading, setLoading] = useState(false);
+
+    const handleSaveChanges = async () => {
+        setLoading(true);
+        console.log({
+            message: "handleSaveChanges",
+            secretKey,
+            webhookSecret,
+        });
+
+        const payload = {
+            project_id: projectId,
+            env_vars: {
+                STRIPE_SECRET_KEY: secretKey,
+                STRIPE_WEBHOOK_SECRET: webhookSecret,
+            },
+            branch: "dev",
+            target: ["preview"],
+            env_vars_comment: {
+                STRIPE_SECRET_KEY: "Stripe Secret Key",
+                STRIPE_WEBHOOK_SECRET: "Stripe Webhook Secret",
+            },
+        };
+        try {
+            const response = await updateEnvs({
+                ...payload,
+                project_id: projectId,
+            });
+            console.log({
+                message: "handleSaveChanges",
+                response,
+            });
+        } catch (error) {
+            console.error("Error updating stripe envs:", error);
+            throw new Error("Failed to update stripe envs");
+        } finally {
+            setLoading(false);
+        }
+    };
 
     return (
         <div className="space-y-3">
@@ -55,6 +108,17 @@ export const StripeSection = () => {
                     onChange={(e) => setWebhookSecret(e.target.value)}
                 />
             </div>
+
+            <Button
+                className="bg-genesoft hover:bg-genesoft/90 text-white text-sm"
+                onClick={handleSaveChanges}
+                disabled={loading}
+            >
+                {loading ? "Saving..." : "Save Changes"}
+                {loading && (
+                    <Loader2 className="w-4 h-4 ml-2 text-white animate-spin" />
+                )}
+            </Button>
         </div>
     );
 };
